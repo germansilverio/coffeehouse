88ac3c42e031406703ea6feba8e9daf42cbcc172
germansilverio <germansilverio91@gmail.com>
2026-07-15 09:25:19 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'coffeehouse-v4';
+const CACHE = 'coffeehouse-v5';
 const SHELL = ['./', 'index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
